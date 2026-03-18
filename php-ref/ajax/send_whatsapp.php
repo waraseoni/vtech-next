84f22722b3066c8888db4bf3e685693c01266b8e
waraseoni <vtech.jbp@gmail.com>
2026-03-19 00:39:50 +0530

    Apply .gitignore

--- php-ref/ajax/send_whatsapp.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config.php';
-if(isset($_POST['job_id'])){
-    $job_id = $_POST['job_id'];
-    // Yahan aap chahe to log save kar sakte ho
-    // Abhi sirf success return kar rahe hain
-    echo "sent";
-}
-?>
--- php-ref/assets/js/low-stock-alert.js
+++
@@ -1,9 +0,0 @@
-// Dashboard pe low stock warning + red badge
-$(document).ready(function(){
-    $.get('ajax/low_stock_check.php', function(data){
-        if(data > 0){
-            $('#low_stock_badge').html(data).show();
-            toastr.error(data + ' items stock 5 se kam hai!');
-        }
-    });
-});
