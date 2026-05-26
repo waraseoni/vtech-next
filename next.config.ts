2935081809a755d00ea357b576a1e179a0c460e2
waraseoni <vtech.jbp@gmail.com>
2026-05-26 10:42:10 +0530

    dash fixes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ import withPWA from "next-pwa";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   turbopack: {},
+  allowedDevOrigins: [
+    '192.168.1.*',   // ghar/office WiFi
+    '192.168.29.*',  // mobile hotspot (optional)
+    '10.0.0.*',      // kuch routers ye range use karte hain (optional)
+  ],
 };
 
 const withPWAConfig = withPWA({
